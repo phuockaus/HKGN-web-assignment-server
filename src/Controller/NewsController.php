3f9472b79a50ed03cea2be46221fb698916cfcce
nhatnam23012000 <nhatnam23012000@gmail.com>
2021-11-27 11:07:39 +0700

    [WA-06] create endpoint for admin

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -21,8 +21,21 @@ class NewsController {
 
   public function processRequest() {
     switch ($this->requestMethod) {
+      case 'OPTIONS':
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+
       case 'GET':
         $response = $this->getAll();
+        break;
+
+      case 'POST':
+        $response = $this->createNews();
+        break;
+
+      case 'PUT':
+        $response = $this->updateNews($this->newsCode);
         break;
 
       default:
@@ -39,6 +52,51 @@ class NewsController {
     $result = $this->newsGateway->getAllNews();
     $response['status_code_header'] = 'HTTP/1.1 200 OK';
     $response['body'] = json_encode($result);
+    return $response;
+  }
+
+  private function createNews() {
+    $_POST = json_decode(file_get_contents("php://input"),true);
+    $input = $_POST;
+    if (! $this->validateInput($input)) {
+      return $this->unprocessableEntityResponse();
+    }
+    $this->newsGateway->createNews($input);
+    $response['status_code_header'] = 'HTTP/1.1 201 Created';
+    $response['body'] = null;
+    return $response;
+  }
+
+  private function updateNews($newsCode) {
+    $result = $this->newsGateway->findNews($newsCode);
+    if ( ! $result) {
+      return $this->notFoundResponse();
+    }
+    $_POST = json_decode(file_get_contents("php://input"),true);
+    $input = $_POST;
+    if (! $this->validateInput($input)) {
+      return $this->unprocessableEntityResponse();
+    }
+    $this->newsGateway->updateNews($newsCode, $input);
+    $response['status_code_header'] = 'HTTP/1.1 200 OK';
+    $response['body'] = null;
+    return $response;
+  }
+
+  private function validateInput($input) {
+    if (! isset($input['title'])) return false;
+    if (! isset($input['image_link'])) return false;
+    if (! isset($input['content'])) return false;
+    if (! isset($input['position'])) return false;
+    return true;
+  }
+
+  private function unprocessableEntityResponse()
+  {
+    $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+    $response['body'] = json_encode([
+      'error' => 'Invalid input'
+    ]);
     return $response;
   }
 
--- src/TableGateways/NewsGateway.php
+++ src/TableGateways/NewsGateway.php
@@ -20,4 +20,71 @@ class NewsGateway {
       exit($e->getMessage());
     }
   }
+
+  public function findNews($newsID) {
+    $statement = "
+      SELECT * FROM news WHERE news_id = ?
+    ";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array($newsID));
+      $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+      return $result;
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
+
+  public function updateNews($newsID, $input) {
+    $statement = "
+      UPDATE news
+      SET
+        title = :title,
+        image_link = :image_link,
+        content = :content,
+        position = :position,
+        updated_at = :updated_at
+      WHERE news_ID = :news_ID
+    ";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array(
+        'title' => $input['title'],
+        'image_link' => $input['image_link'],
+        'content' => $input['content'],
+        'position' => $input['position'],
+        'updated_at' => date('Y-m-d H:i:s', time()),
+        'news_ID' => (int)$newsID        
+      ));
+      return $statement->rowCount();
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
+
+  public function createNews($input) {
+    $statement = "
+      INSERT INTO news
+        (title, image_link, content, position, created_at, updated_at)
+      VALUES
+        (:title, :image_link, :content, :position, :created_at, :updated_at)
+    ";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array(
+        'title' => $input['title'],
+        'image_link' => $input['image_link'],
+        'content' => $input['content'],
+        'position' => (int)$input['position'],
+        'created_at' => date('Y-m-d H:i:s', time()),
+        'updated_at' => date('Y-m-d H:i:s', time())
+      ));
+      return $statement->rowCount();
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
 }
